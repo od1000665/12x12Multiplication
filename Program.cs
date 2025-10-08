bcbdda158e005ff953211e4ad0925c5ba21b68c2
od1000665 <od1000665@otc.edu>
2025-10-07 19:57:16 -0500

    pseudocode
    pseudocode

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 12x12 Multiplication Table
+    Created by: Owen Douglas
+    The program will print out a 12x12 multiplication table
+*/
+
+//Outer loop to create the rows
+
+
+//Inner loop to create columns
+
+
+//display the rows and columns
+
+
+//Print | at the end of each row
+
+
+//Print ------ for inbetween each row
